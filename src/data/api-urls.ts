3ea1dec60f0fc98d70baee5c366466babbfe1a48
Vladyslav Orlenko <vlad.pos1bl@gmail.com>
2023-10-30 21:01:36 +0200

    edit url path

--- src/data/api-urls.ts
+++ src/data/api-urls.ts
@@ -1,3 +1,5 @@
-export const PRODUCTS_URL = '/_new/products.json';
-export const PRODUCT_DETAILS_URL = '/_new/products/';
+// eslint-disable-next-line
+export const PRODUCTS_URL = 'https://mate-academy.github.io/react_phone-catalog/_new/products.json';
+// eslint-disable-next-line
+export const PRODUCT_DETAILS_URL = 'https://mate-academy.github.io/react_phone-catalog/_new/products/';
 export const JSON_URL = '.json';
